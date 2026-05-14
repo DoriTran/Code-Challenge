09145c8093144d80e853142e01d095cea06f78db
DoriTran <tranquocdong308@gmail.com>
2026-05-14 12:02:09 +0700

    finish problem 1

---
+++ Problem 1/problem_1.js
@@ -0,0 +1,22 @@
+// 1. Using a loop
+var sum_to_n_a = function (n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+// 2. Using recursion
+var sum_to_n_b = function (n) {
+  if (n <= 1) return n;
+
+  return n + sum_to_n_b(n - 1);
+};
+
+// 3. Using mathematical formula
+var sum_to_n_c = function (n) {
+  return (n * (n + 1)) / 2;
+};
